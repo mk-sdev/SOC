070a1ad3d5cf7429ba1a8422415f903bb42ce0bd
Mikołaj <kaleta222@gmail.com>
2026-01-12 13:01:39 +0100

    implement collect.sh for collecting nginx logs

--- collector/collect.sh
+++ collector/collect.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "collect.sh"
+# Ścieżki w kontenerze soc
+LOG_SOURCE="./logs/access.log"
+OUTPUT="./logs/collected.log"
+
+# Tworzymy plik jeśli nie istnieje
+touch $OUTPUT
+
+echo "[*] Starting log collection from $LOG_SOURCE..."
+tail -Fn0 $LOG_SOURCE | while read line; do
+    echo "$line" >> $OUTPUT
+done
