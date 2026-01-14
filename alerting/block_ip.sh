be1c41249a99a3fe544f8d536bf66c1472865497
Mikołaj <kaleta222@gmail.com>
2026-01-14 09:15:24 +0100

    implement blobk_ip.sh

--- alerting/block_ip.sh
+++ alerting/block_ip.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "block_ip.sh"
+if [ -z "$1" ]; then
+    echo "Usage: $0 <IP>"
+    exit 1
+fi
+
+IP=$1
+echo "[*] Blocking IP $IP..."
+iptables -A INPUT -s $IP -j DROP
+echo "[✓] IP $IP blocked."
