ca3f570af42db074c54e0fbf3c1fd50b2aa5e9f9
Mikołaj <kaleta222@gmail.com>
2026-01-23 10:55:31 +0100

    improve clear_logs script

--- logs/clear_logs.sh
+++ logs/clear_logs.sh
@@ -2,11 +2,28 @@
 
 # execute this only from the root directory on the host
 
-shopt -s nullglob # the loops should execute even if there are no files in the dir
+shopt -s nullglob  ## the loops should execute even if there are no files in the dir
 
-for file in ./logs/*.{log,csv}; do
+LOG_DIR="./logs"
+
+FILES=()
+
+if [ $# -eq 0 ]; then
+    # clear all files if no arguments provided
+    FILES=("$LOG_DIR"/*.{log,csv})
+else
+    # match fragments to file names
+    for arg in "$@"; do
+        matches=("$LOG_DIR/"*"$arg"*".log" "$LOG_DIR/"*"$arg"*".csv")
+        FILES+=("${matches[@]}")
+    done
+fi
+
+for file in "${FILES[@]}"; do
     if [ -f "$file" ]; then
         echo "Clearing file: $file"
         > "$file"
+    else
+        echo "No matching file for: $file"
     fi
 done
