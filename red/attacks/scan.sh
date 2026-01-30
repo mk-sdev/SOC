7c6c3c4d8a589a6b246515ee3dc4eeb014ab8be4
Mikołaj <kaleta222@gmail.com>
2026-01-30 10:11:27 +0100

    add read_team user to red container

--- red/attacks/scan.sh
+++ red/attacks/scan.sh
@@ -8,6 +8,6 @@ fi
 
 echo "[*] Simulating nmap scan..."
 # enforcing http
-nmap -p 80 --script=http-title --source-port 53 -sS $TARGET
+nmap -p 80 --script=http-title --source-port 53 -sT $TARGET
 
 echo "[*] Finished simulating nmap scan"
